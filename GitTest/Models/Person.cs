715026846c0dcecbdc49faa7e4f9c440d6b02c0a
Niloofar <nikitmb2@gmail.com>
2021-05-17 10:15:44 +0430

    add invite code property

--- GitTest/Models/Person.cs
+++ GitTest/Models/Person.cs
@@ -10,5 +10,6 @@ namespace GitTest.Models
         public int Id { get; set; }
         public string UserName { get; set; }
         public int Age { get; set; }
+        public string InviteCode { get; set; }
     }
 }
